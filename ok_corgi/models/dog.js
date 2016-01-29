95a2cdff34cee899d0b76fab0c80be82eb492de4
David Keathley <keathleydavidj@gmail.com>
2016-01-29 11:41:40 -0600

    everything works, all done

--- ok_corgi/models/dog.js
+++ ok_corgi/models/dog.js
@@ -1,12 +1,12 @@
 // Dog model w/ Mongoose
 var mongoose = require('mongoose');
 
-var dogSchema = new moongoose.Schema({
+var dogSchema = new mongoose.Schema({
   // dog: {type:String, required: true },
   name: {type:String, required: true},
   photo_url: {type:String, required: true},
   like: {type:Boolean }
 });
 
-var Dog = moongoose.model('Dog', dogSchema);
+var Dog = mongoose.model('Dog', dogSchema);
 module.exports = Dog;
--- ok_corgi/routes/index.js
+++ ok_corgi/routes/index.js
@@ -14,37 +14,40 @@ function randomDog(dogArray) {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // name = req.params.name || "Friend";
+  name = req.params.name || "Friend";
   // var color = randomColor();
 
   // USE MONGOOSE TO GET A RANDOM DOG FROM THE DATABASE, THEN RENDER THE VIEW IN THE DATABASE CALLBACK
 
-  Dog.find({}, 'dog', function(err, data) {
+  Dog.find({}, function(err, data) {
     if (err) console.log(err);
     var random = randomDog(data);
-    console.log(random.dog);
-    res.render('index', { title: 'OKCorgi', dog: random.dog });
+    console.log(random.name);
+    res.render('index', { title: 'OKCorgi', name: random.name, photo: random.photo_url });
   });
 });
 
 /* GET dog form. */
 router.get('/dog_form', function(req, res, next) {
-  var color = randomColor();
+  // var color = randomColor();
   res.render('dog_form', { title: 'OKCorgi' });
 });
 
   // USE MONGOOSE TO SAVE A NEW DOG TO THE DATABASE, THEN REDIRECT TO THE ROOT URL
 router.post('/', function(req, res, next) {
-  var newDog = req.body.dog;
+  var dogName = req.body.name;
+  var dogPhoto = req.body.photo_url;
 
   var newDog = Dog({
-    dog: newDog
+    name: dogName,
+    photo_url: dogPhoto
   });
 
   newDog.save(function(err){
     if (err) console.log(err);
 
-    res.redirect('/');
+    // res.redirect('/');
+    res.send('Dog Saved!');
   })
 
 });
